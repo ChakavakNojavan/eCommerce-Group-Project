157fa54eb2cf8dbba704fe257d5a306c63d38366
“Chakavak <chakavak.nojavan@gmail.comuser.email chakavak.nojavan@gmail.com>
2023-03-21 11:41:02 -0400

    chaka-comment

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 function App() {
   const [bacon, setBacon] = useState(null);
 
   useEffect(() => {
-    fetch('/bacon')
-      .then(res => res.json())
-      .then(data => setBacon(data));
+    fetch("/bacon")
+      .then((res) => res.json())
+      .then((data) => setBacon(data));
   }, []);
 
   return <div>{bacon ? bacon : `...where's my stuff?...`}</div>;
 }
 
 export default App;
+
+//this is a comment by chaka
