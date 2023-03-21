72cbb3d1e004d7a807ca294bb5b338348d05109b
“Brandon <brandon.a.ri@hotmail.com>
2023-03-21 15:21:49 -0400

    Bare bones app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,19 @@
-import {react } from "react"
-import { BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    //all the routes can change, I just put them as place holders, as well as the elements!
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element="" />
+        <Route path="/about" element="" />
+        <Route path="/products" element="" />
+        <Route path="/products/:_id" element="" />
+        <Route path="/cart" element="" />
+        <Route path="" element={<h1>404: Oops!</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
-
-
