f6f2f053ff7393faa616fb119a755133968b9ea1
Jon Khamidov <jon.khamidov@gmail.com>
2023-03-22 13:21:01 -0400

    update to single product page w/o working button

--- client/src/components/SingleProduct.js
+++ client/src/components/SingleProduct.js
@@ -1,60 +1,132 @@
 import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
+
 
 const SingleProduct = () => {
+    const {_id} = useParams()
+    const [watch, setWatch] =  useState()
+
+    useEffect( () => {
+        fetch(`/api/products/${_id}`)
+            .then(res => res.json())
+            .then( (data) => {
+                setWatch(data)
+                console.log(data)
+            })
+            .catch( (error) => {
+                console.log(error)
+            })
+    }, [])
+
 return (
-    <Body>
-    
-    <Wrapper>
+    <div>
+        {!watch
+        ?(<div>
+            LOADING ICON
+        </div>)
+        :(
+            <Wrapper>
 
         <div>
-            <p>this is an img</p> 
+            <WatchImg src={watch.imageSrc}/>
         </div>
 
     <InfoSection>
-        <h2>Product Name</h2>
-        <Price>$20.00</Price>
+        <WatchName>{watch.name}</WatchName>
+        <Price>{watch.price}</Price>
+        <Stock>Stock: {watch.numInStock}</Stock>
+        <Category>Category: {watch.category}</Category>
         <p>This is the best watch on the market I promise you. It can even tell the time when you are asleep</p>
         <AddToCart>Add to cart</AddToCart>
     </InfoSection>
     
     </Wrapper>
+        )}
+    </div>
     
-    </Body>
 )
 }
 
-const Body = styled.body`
-height: 100vh;
+const Stock = styled.p`
+margin:0;
+`
+const Category = styled.p`
+margin:0;
+`
+const Description = styled.p`
 margin: 0;
-`;
+`
 const WatchImg = styled.img`
 height: 500px;
 width: auto;
+border: solid black 1px;
+margin-right: 50px;
+border-radius: 30px;
+:hover {
+    transform: scale(1.5);
+}
 `
 const Wrapper = styled.div`
 display: flex;
 align-items: center;
 justify-content: center;
 height: 100%;
+
 `
 const InfoSection = styled.div`
 display: flex;
 flex-direction: column;
 justify-content: center
 `
-const AddToCart =  styled.button`
-max-width: 155px;
-height: 50px;
-font-size: 1.25em;
-background-color: black;
-color: white;
+const AddToCart = styled.button`
+align-items: center;
+background-color: rgba(240, 240, 240, 0.26);
+border: 1px solid #DFDFDF;
+border-radius: 16px;
+box-sizing: border-box;
+color: #000000;
+cursor: pointer;
+display: flex;
+font-family: Inter, sans-serif;
+font-size: 18px;
+justify-content: center;
+line-height: 28px;
+max-width: 100%;
+padding: 14px 22px;
+text-decoration: none;
+transition: all .2s;
+user-select: none;
+-webkit-user-select: none;
+touch-action: manipulation;
+width: 150px;
+
+
+:active,
+:hover {
+outline: 0;
+}
+
+:hover {
+background-color: #FFFFFF;
+border-color: rgba(0, 0, 0, 0.19);
+}
+
+@media (min-width: 100px) 
+{
+    font-size: 20px;
+    min-width: 200px;
+    padding: 14px 16px;
+}
 `
-const Price =  styled.h2`
-color: red
+const Price = styled.h2`
+color: red;
+margin-bottom: 50px;
 `
-
-
-
+const WatchName = styled.h2`
+margin-bottom: 40px;
+` 
 
 
 export default SingleProduct
