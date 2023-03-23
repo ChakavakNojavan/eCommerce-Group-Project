b9ca0400ba589b26b489db5419baa29a8d4ee6c0
“Brandon <brandon.a.ri@hotmail.com>
2023-03-23 10:48:13 -0400

    Loading element

--- client/src/components/Affiliates.js
+++ client/src/components/Affiliates.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { defer } from "react-router-dom";
 import styled from "styled-components";
 
 const Affiliates = () => {
+  useEffect(()=> {
+    fetch("/api/companies")
+    .then((response) => response.json())
+  })
   return <></>;
 };
 
